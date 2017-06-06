79a329b288dad28a0077de1063d7eb1c62b72d37
15210240070 <15210240070@fudan.edu.cn>
2017-06-06 12:14:33 +0800

    Initial commit

---
+++ src/main/java/cn/edu/fudan/DataqualityApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.fudan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataqualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataqualityApplication.class, args);
+	}
+}
